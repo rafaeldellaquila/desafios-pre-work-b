63986acfcbcdfcad0802533bd0eb891df346b864
rafaeldellaquila <rafadellaquila@gmail.com>
2021-08-18 23:59:51 -0300

    Outra solução para  a criação dos objetos

--- dia2/src/immutable.js
+++ dia2/src/immutable.js
@@ -13,3 +13,16 @@ const jane = {
 
 console.log("John:", john);
 console.log("Jane:", jane);
+
+
+//Outra Solução possivel
+function Person(name, surname, age, hobbies) {
+  this.name = name,
+    this.surname = surname,
+    this.age = age,
+    this.hobbies = hobbies;
+}
+
+const pedro = new Person('Pedro', 'De Lara', 30, ["Surf", "Design","MuayThai", "Programming"]);
+
+console.log(pedro);
