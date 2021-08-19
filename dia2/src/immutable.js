149ea73e6389fd5eb624fda6d330b3c7c09f363d
rafaeldellaquila <rafadellaquila@gmail.com>
2021-08-19 00:00:16 -0300

    Só comitando a identação

--- dia2/src/immutable.js
+++ dia2/src/immutable.js
@@ -18,9 +18,9 @@ console.log("Jane:", jane);
 //Outra Solução possivel
 function Person(name, surname, age, hobbies) {
   this.name = name,
-    this.surname = surname,
-    this.age = age,
-    this.hobbies = hobbies;
+  this.surname = surname,
+  this.age = age,
+  this.hobbies = hobbies;
 }
 
 const pedro = new Person('Pedro', 'De Lara', 30, ["Surf", "Design","MuayThai", "Programming"]);
