029ff6a873a526da36e95837873b4d469f9aff5b
rafaeldellaquila <rafadellaquila@gmail.com>
2021-08-19 20:06:54 -0300

    Solução exercicio 02

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -20,3 +20,45 @@ function nameCase(name) {
     return `${name[0].toUpperCase()}${name.slice(1).toLowerCase()}`;
   }
 }
+
+///////////
+const form = document.querySelector('[data-js="form"]');
+const select = document.createElement("select");
+
+const colors = {
+  amarelo: "#FF3",
+  rosa: "#DAF",
+  laranja: "#E74",
+  azul: "#33D",
+  vermelho: "#F03",
+};
+
+const container = document.createElement("div");
+container.style.display = "flex";
+
+function divColor(color) {
+  const div = document.createElement("div");
+  div.style.width = "100px";
+  div.style.height = "100px";
+  div.style.background = color;
+  return div;
+}
+
+for (let key in colors) {
+  const selectColor = document.createElement("option");
+  selectColor.value = colors[key];
+  selectColor.textContent = key;
+  select.appendChild(selectColor);
+}
+
+select.addEventListener("change", (e) => {
+  container.innerHTML = "";
+  Array.from(e.target.selectedOptions).forEach((option) => {
+    const div = divColor(option.value);
+    container.appendChild(div);
+  });
+});
+
+select.setAttribute("multiple", "");
+form.appendChild(select);
+document.body.appendChild(container);
