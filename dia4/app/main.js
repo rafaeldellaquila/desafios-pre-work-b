43c6fdcac8e7ec30f5b03eeec866ab33f8b9f378
rafaeldellaquila <rafadellaquila@gmail.com>
2021-08-20 14:38:37 -0300

    Exercicio 2, parte dele

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,1 +1,18 @@
-import './style.css'
+import "./style.css";
+const title = document.querySelector("h1");
+const tbody = document.querySelector("tbody");
+const div = document.createElement("div");
+
+fetch("http://localhost:3333", { method: "GET" })
+  .then((result) => result.json())
+  .then((result) => (title.textContent = result.message));
+
+fetch("http://localhost:3333/cars", { method: "GET" })
+  .then((result) => result.json())
+  .then((result) => {
+    if (result.length <= 0) {
+      div.textContent = "Nenhum carro encontrado";
+    }
+  });
+
+tbody.appendChild(div);
